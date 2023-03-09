6fa19b84614190562f1f88486274460177d199c7
Gabriel <gbarros0495@gmail.com>
2023-03-09 20:48:31 -0300

    Java Queue

---
+++ EqualsHashcode/src/EqualsHashcode/Main.java
@@ -0,0 +1,37 @@
+package EqualsHashcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        /*List<Carro> listaCarros = new ArrayList<>();
+
+        listaCarros.add(new Carro("Ford"));
+        listaCarros.add(new Carro("Chevrolet"));
+        listaCarros.add(new Carro("Volkswagen"));
+        System.out.println(listaCarros.contains(new Carro("Ford")));
+        System.out.println(new Carro("Ford").hashCode());
+
+        Carro carro1 = new Carro("Ford");
+        Carro carro2 = new Carro("Ford");
+
+        System.out.println(carro1.equals(carro2));*/
+
+        /*Stack<Carro> stackCarro = new Stack<>();
+        stackCarro.push(new Carro("Ford"));
+        stackCarro.push(new Carro("Chevrolet"));
+        stackCarro.push(new Carro("Fiat"));
+
+        System.out.println(stackCarro);
+        System.out.println(stackCarro.pop());
+        System.out.println(stackCarro);
+
+        System.out.println(stackCarro.peek());
+        System.out.println(stackCarro);
+
+        System.out.println(stackCarro.empty());*/
+
+    }
+}
